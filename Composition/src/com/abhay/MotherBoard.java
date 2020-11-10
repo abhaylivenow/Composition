d14fef227250dd784d6b200319d2ecb13d8a229c
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2020-11-10 18:24:27 +0530

    Add files via upload

---
+++ Composition/src/com/abhay/MotherBoard.java
@@ -0,0 +1,35 @@
+package com.abhay;
+
+public class MotherBoard {
+    private String ModelName;
+    private String Manufacturer;
+    private int ramSlots;
+    private int cardSlots;
+
+    public MotherBoard(String modelName, String manufacturer, int ramSlots, int cardSlots) {
+        ModelName = modelName;
+        Manufacturer = manufacturer;
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+    }
+
+    public void loadProgram(String ProgramName) {
+        System.out.println(ProgramName + " is loading....");
+    }
+
+    public String getModelName() {
+        return ModelName;
+    }
+
+    public String getManufacturer() {
+        return Manufacturer;
+    }
+
+    public int getRamSlots() {
+        return ramSlots;
+    }
+
+    public int getCardSlots() {
+        return cardSlots;
+    }
+}
---
+++ Composition/src/com/abhay/PC.java
@@ -0,0 +1,25 @@
+package com.abhay;
+
+public class PC {
+    private Case theCase;
+    private Monitor theMonitor;
+    private MotherBoard theMotherBoard;
+
+    public PC(Case theCase, Monitor theMonitor, MotherBoard theMotherBoard) {
+        this.theCase = theCase;
+        this.theMonitor = theMonitor;
+        this.theMotherBoard = theMotherBoard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getTheMonitor() {
+        return theMonitor;
+    }
+
+    public MotherBoard getTheMotherBoard() {
+        return theMotherBoard;
+    }
+}
